d2dd32faf97898ac939c55a7fbb3c77b34b795e5
marcouras <30621717+marcouras@users.noreply.github.com>
2025-05-12 11:15:27 +0200

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,6 @@ class TextInput(BaseModel):
     testo: str
 
 @app.get("/sentiment")
-def analizza_sentiment(input: TextInput):
+def analizza_sentiment():
     # mock output (finto)
     return {"sentiment": "positivo", "confidence": 0.92}
