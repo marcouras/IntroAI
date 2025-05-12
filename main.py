af77937157cf7a6495ee3ee3200070a5ddd2e194
marcouras <30621717+marcouras@users.noreply.github.com>
2025-05-12 11:12:47 +0200

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ app = FastAPI()
 class TextInput(BaseModel):
     testo: str
 
-@app.post("/sentiment")
+@app.get("/sentiment")
 def analizza_sentiment(input: TextInput):
     # mock output (finto)
     return {"sentiment": "positivo", "confidence": 0.92}
