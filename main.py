25964db576e78a6990e4a21bf05e7e0794671db3
marcouras <30621717+marcouras@users.noreply.github.com>
2025-05-12 13:15:51 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+import requests
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 
 class TextInput(BaseModel):
     testo: str
-
+    
+@app.get("/uniss")
+def proxy():
+    
+    r = requests.get("https://www.uniss.it/")
+    return HTMLResponse(content=r.text, status_code=200)
+    
 @app.get("/sentiment")
 def analizza_sentiment():
     # mock output (finto)
